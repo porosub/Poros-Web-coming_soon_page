39f51e42dac499844cfb167a3c420cd21702a5d1
Nafibal <baliya363@gmail.com>
2023-10-05 13:51:36 +0700

    chore: add icon

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,9 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="shortcut icon" href="/favicon.ico" sizes="any" />
-      </Head>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
